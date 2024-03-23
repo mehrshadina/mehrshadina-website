6c410820fd94ff6dfe8d7497a96039223c9eae1d
mehrshadina <mehrshadinaa@gmail.com>
2024-03-23 13:03:54 +0330

    post archives fixed.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 # blog/urls.py
 from django.urls import path
-from .views import post_list, post_detail
+from .views import post_list, post_detail, post_archive
 
 urlpatterns = [
     path('', post_list, name='post_list'),
     path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('archive/<int:year>/<int:month>/', post_archive, name='post_archive'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,12 +2,32 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.http import HttpResponseRedirect
 from blog.forms import CommentForm
+from django.db.models import Count
+from django.db.models.functions import TruncMonth
 
 def post_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_archive.html', {'posts': posts})
+
+def post_archive(request, year, month):
+    # Filter posts by year and month
+    posts = Post.objects.filter(pub_date__year=year, pub_date__month=month)
+    
+    # Fetch archives for navigation
+    archives = Post.objects.annotate(
+        year_month=TruncMonth('pub_date')
+    ).values('year_month').annotate(
+        count=Count('id')
+    ).order_by('-year_month')
+    
+    context = {
+        "posts": posts,
+        "archives": archives,
+    }
+    return render(request, 'blog/post_archive.html', context=context)
 
 def post_detail(request, pk):
+    recent_posts = Post.objects.values_list('title', flat=True)
     post = Post.objects.get(pk=pk)
     form = CommentForm()
     if request.method == "POST":
@@ -21,9 +41,17 @@ def post_detail(request, pk):
             comment.save()
             return HttpResponseRedirect(request.path_info)
     
+    archives = Post.objects.annotate(
+        year_month=TruncMonth('pub_date')
+    ).values('year_month').annotate(
+        count=Count('id')
+    ).order_by('-year_month')
+
     comments = Comment.objects.filter(post=post)
     context = {
         "post": post,
+        "recent_posts": recent_posts,
+        "archives": archives,
         "comments": comments,
         "form": CommentForm(),
     }
